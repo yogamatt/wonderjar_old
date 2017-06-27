a006eaaadf3a1d428b9dc810f43ef694729200fb
Matthew Ediger <edigermatthew@gmail.com>
2017-06-26 23:35:45 -0600

    Add files via upload

---
+++ templates/homepage.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Wonderjar Template - Homepage
+ * @author Matt
+ * @category template
+ * @version 1.0
+ * @since 2017-03-29
+ *
+ */
+
+
+/*
+ * @function wj_get_homepage()
+ * returns: `page_id`, `page_time`, `page_special`, `page_title`, `page_content`
+ */
+
+wj_get_homepage();
+
+
+// add homepage to bodyclass
+$bodyclass .= 'homepage ';
+
+// add extra stylesheets for plugins
+extra_stylesheets();
+
+// include header
+include ($_SERVER['DOCUMENT_ROOT'].'/header.php');
+echo '<!-- End Header -->';
+
+?>
+
+
+<div class="homepage-content">
+
+	<section class="homepage-branding">
+		<?php include ($_SERVER['DOCUMENT_ROOT'] . '/templates/template-parts/homepage/homepage-branding.php'); ?>
+	</section>
+
+	<section class="plugin-area">
+		<div class="feature-plugin">
+			<div class="inner-container">
+				<div class="col-container">
+					<?php call_shortcode('feature-01'); ?>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="homepage-mce-content">
+		<div class="inner-container">
+			<div class="col-container">
+				<?php echo $page_content; ?>
+			</div>
+		</div>
+	</section>
+
+</div>
+
+
+<?php
+
+// include footer
+include ($_SERVER['DOCUMENT_ROOT'].'/footer.php');
+echo '<!-- End Footer -->';
+
+?>
---
+++ templates/template-parts/homepage/homepage-branding.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Wonderjar Template Part - Homepage Branding
+ * @author Matt
+ * @category admin, template-part
+ * @version 1.0
+ * @since 2017-06-06
+ *
+ */
+
+ ?>
+
+
+ <div class="homepage-tagline">
+	<div class="inner-container">
+		<div class="col-container">
+			<div class="col-6">
+				<?php return_tagline(); echo $tagline_content; ?>
+			</div>
+			<div class="col-6 full-image">
+				<img src="../images/uploads/interior/manthinking.png" width="300" height="400" />
+			</div>
+		</div>
+	</div>
+</div>
+<div class="homepage-promo">
+	<div class="inner-container">
+		<div class="col-container">
+			<?php return_promo(); echo $promo_content; ?>
+		</div>
+	</div>
+</div>
